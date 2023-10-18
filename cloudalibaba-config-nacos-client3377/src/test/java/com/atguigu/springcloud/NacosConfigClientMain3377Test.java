06b1dcd87e0b24e1d8e261e9abae5e9036b5972a
moments <572836448@qq.com>
2023-10-18 18:35:03 +0800

    nacos

---
+++ cloudalibaba-config-nacos-client3377/src/test/java/com/atguigu/springcloud/NacosConfigClientMain3377Test.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class NacosConfigClientMain3377Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public NacosConfigClientMain3377Test(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( NacosConfigClientMain3377Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order83/src/test/java/com/atguigu/springcloud/OrderNacosMain83Test.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class OrderNacosMain83Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public OrderNacosMain83Test(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( OrderNacosMain83Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
